782fa0c7f41ba3809f00e86db03ab9ba699145c3
sergioduarte1991-ship-it <sergioduarte.1991@gmail.com>
2026-02-24 12:13:28 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+﻿const CACHE_NAME = 'calc-ftth-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar y guardar en caché
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Responder desde la caché cuando no hay conexión
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
+
